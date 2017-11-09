b8b036df39173e16f24626db8fac78673b205163
John Andersen <johnandersenpdx@gmail.com>
2017-11-09 01:39:32 -0800

    Formatting and stopped work on USART for now
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- src/main.c
+++ src/main.c
@@ -4,77 +4,57 @@
 */
 #include <processor.h>
 
-void delay(int);
-
-void delay(int dly)
-{
-  while( dly--);
+void delay(int dly) {
+  while (dly--);
 }
 
-void initClock()
-{
-// This is potentially a dangerous function as it could
-// result in a system with an invalid clock signal - result: a stuck system
-        // Set the PLL up
-        // First ensure PLL is disabled
-        RCC_CR &= ~BIT24;
-        while( (RCC_CR & BIT25)); // wait for PLL ready to be cleared
-        // set PLL multiplier to 12 (yielding 48MHz)
+void clock_init() {
+  // This is potentially a dangerous function as it could
+  // result in a system with an invalid clock signal - result: a stuck system
+  // Set the PLL up
+  // First ensure PLL is disabled
+  RCC_CR &= ~BIT24;
+  while( (RCC_CR & BIT25)); // wait for PLL ready to be cleared
+  // set PLL multiplier to 12 (yielding 48MHz)
   // Warning here: if system clock is greater than 24MHz then wait-state(s) need to be
-        // inserted into Flash memory interface
-        FLASH_ACR |= BIT0;
-        FLASH_ACR &=~(BIT2 | BIT1);
+  // inserted into Flash memory interface
+  FLASH_ACR |= BIT0;
+  FLASH_ACR &=~(BIT2 | BIT1);
 
-        // Turn on FLASH prefetch buffer
-        FLASH_ACR |= BIT4;
-        RCC_CFGR &= ~(BIT21 | BIT20 | BIT19 | BIT18);
-        RCC_CFGR |= (BIT21 | BIT19 );
+  // Turn on FLASH prefetch buffer
+  FLASH_ACR |= BIT4;
+  RCC_CFGR &= ~(BIT21 | BIT20 | BIT19 | BIT18);
+  RCC_CFGR |= (BIT21 | BIT19 );
 
-        // Need to limit ADC clock to below 14MHz so will change ADC prescaler to 4
-        RCC_CFGR |= BIT14;
+  // Need to limit ADC clock to below 14MHz so will change ADC prescaler to 4
+  RCC_CFGR |= BIT14;
 
-// Do the following to push HSI clock out on PA8 (MCO)
-// for measurement purposes.  Should be 8MHz or thereabouts (verified with oscilloscope)
-/*
-        RCC_CFGR |= ( BIT26 | BIT24 );
-        RCC_AHBENR |= BIT17;
-        GPIOA_MODER |= BIT17;
-*/
-
-        // and turn the PLL back on again
-        RCC_CR |= BIT24;
-        // set PLL as system clock source
-        RCC_CFGR |= BIT1;
+  // Turn the PLL back on again
+  RCC_CR |= BIT24;
+  // Set PLL as system clock source
+  RCC_CFGR |= BIT1;
 }
 
-void configPins()
-{
+void config_pins() {
 	// Power up PORTB
 	RCC_AHBENR |= BIT18;
-
 }
 
-int main()
-{
-	initClock();
-	unsigned i=0;
-	configPins();
+int main() {
+	unsigned int i = 0;
+
+	clock_init();
+	config_pins();
+
 	GPIOB_MODER |= BIT6; // make bit3  an output
 	GPIOB_MODER &= ~BIT7; // make bit3  an output
-	while(1)
-	{
+
+	while(1) {
 		GPIOB_ODR |= BIT3;
-		delay(200000);
+		delay(2000000);
 		GPIOB_ODR &= ~BIT3;
-		delay(200000);
+		delay(2000000);
 	}
+
 	return 0;
 }
-
-
-
-
-
-
-
-
--- src/uart.c
+++ src/uart.c
@@ -2,27 +2,45 @@
 #include <stdio.h>
 #include <processor.h>
 
+/*
+Good sure here in  A.19.3 here: http://www.st.com/content/ccc/resource/technical/document/reference_manual/c2/f8/8a/f2/18/e6/43/96/DM00031936.pdf/files/DM00031936.pdf/jcr:content/translations/en.DM00031936.pdf
+#define USART1_CR1	REGISTER_32(USART1_BASE + 0)
+#define USART1_CR2	REGISTER_32(USART1_BASE + 4)
+#define USART1_CR3	REGISTER_32(USART1_BASE + 8)
+#define USART1_BRR	REGISTER_32(USART1_BASE + 0x0c)
+#define USART1_GTPR	REGISTER_32(USART1_BASE + 0x10)
+#define USART1_RTOR	REGISTER_32(USART1_BASE + 0x14)
+#define USART1_RQR	REGISTER_32(USART1_BASE + 0x18)
+#define USART1_ISR	REGISTER_32(USART1_BASE + 0x1c)
+#define USART1_ICR	REGISTER_32(USART1_BASE + 0x20)
+#define USART1_RDR	REGISTER_32(USART1_BASE + 0x24)
+#define USART1_TDR	REGISTER_32(USART1_BASE + 0x28)
+*/
+
 uint32_t uart_init() {
   // All inputs except pin 2 (which is the transmit pin).
   writel(GPIOA_BASE + 0x00, 0x44444b44);
   writel(GPIOA_BASE + 0x04, 0x44444444);
   // Enable USART, Transmit, and Receive
-  writel(USART1_BASE + 0x0c, CR1_ENABLED);
+  USART1_BRR |= CR1_ENABLED;
 
-  if (!(readl(USART1_BASE + 0x00) & 0x020)) {
+  /* (1) Oversampling by 16, 9600 baud */
+  /* (2) 8 data bit, 1 start bit, 1 stop bit, no parity */
+  USART1_BRR = 480000 / 96; /* (1) */
+  // USART1_CR1 = USART_CR1_TE | USART_CR1_UE; /* (2) */
+
+  if (!(USART1_CR1 & 0x020)) {
     return -1;
   }
 }
 
 void uart_send(uint8_t send_byte) {
-  writel(USART1_BASE + 0x04, send_byte);
+  USART1_TDR = send_byte;
 }
 
 uint8_t uart_recv() {
   uint32_t status;
   // TODO: Add a timeout to avoid infinite loop
-  do {
-      status = readl(USART1_BASE + 0x00);
-  } while((status & 0x020) == 0);
-  return (readl(USART1_BASE + 0x04) & 0xFF);
+  while ((USART1_CR1 & 0x020) == 0);
+  return (USART1_CR1 & 0xFF);
 }
