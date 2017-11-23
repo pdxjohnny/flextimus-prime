c9058030ea01f01cf5e94d60632d42569e34620b
Julian <sjulian@pdx.edu>
2017-11-22 19:57:43 -0800

    Updated and added files
    Added the rest of the blinky files for ease of use.
    
    Updated test.c code which has the basic structure and functions for the code.
    Updated main.c (Edited Blinky main) for simple hardware testing
    
    I took some of the functions from test.c and put them in main.c just for hardware testing purposes. This main.c is still basically the Blinky main.c but changed. (Though most of what is in the blinky program is in the test.c in terms of initialization and such.

--- Test Code/test.c
+++ Test Code/test.c
@@ -14,16 +14,20 @@
 #include "stm32f042.h"
 
 // Variable to hold pause status
-bool PauseState;
+int PauseState;
 
 // 0 = OFF, 1 = Recording MAX, 2 = Recording MIN
 int ConfigState;
 
 // Variable to hold buzzer status
-bool BuzzerState;
+int BuzzerState;
 
 void PinSetup()
 {
+  // Turn on PortA & PortB
+  RCC_AHBENR |= BIT17;
+	RCC_AHBENR |= BIT18;
+
   // Set Inputs
   GPIOA_MODER |= BIT0; // Flex Sensor
   GPIOA_MODER &= ~BIT1; // "" A0
@@ -42,28 +46,78 @@ void PinSetup()
 
 }
 
+void initClock()
+{
+// This is potentially a dangerous function as it could
+// result in a system with an invalid clock signal - result: a stuck system
+        // Set the PLL up
+        // First ensure PLL is disabled
+        RCC_CR &= ~BIT24;
+        while( (RCC_CR & BIT25)); // wait for PLL ready to be cleared
+        // set PLL multiplier to 12 (yielding 48MHz)
+  // Warning here: if system clock is greater than 24MHz then wait-state(s) need to be
+        // inserted into Flash memory interface
+        FLASH_ACR |= BIT0;
+        FLASH_ACR &=~(BIT2 | BIT1);
+
+        // Turn on FLASH prefetch buffer
+        FLASH_ACR |= BIT4;
+        RCC_CFGR &= ~(BIT21 | BIT20 | BIT19 | BIT18);
+        RCC_CFGR |= (BIT21 | BIT19 ); 
+
+        // Need to limit ADC clock to below 14MHz so will change ADC prescaler to 4
+        RCC_CFGR |= BIT14;
+
+// Do the following to push HSI clock out on PA8 (MCO)
+// for measurement purposes.  Should be 8MHz or thereabouts (verified with oscilloscope)
+/*
+        RCC_CFGR |= ( BIT26 | BIT24 );
+        RCC_AHBENR |= BIT17;
+        GPIOA_MODER |= BIT17;
+*/
+
+        // and turn the PLL back on again
+        RCC_CR |= BIT24;
+        // set PLL as system clock source
+        RCC_CFGR |= BIT1;
+}
+
+
+/*
 // IRQ handler for flex sensor interrupts
 int FlexSensorIRQ()
 {
-  if (interrupt from sensor)
+  if (GPIOA_IDR & BIT0)
     Buzzer();
 
   else
     return 0;
 }
-
+*/
 // IRQ handler for both button interrupts
 int ButtonIRQ()
 {
-  if (pause button)
+  if (GPIOB_IDR & BIT3)
+  {
+    while (GPIOB_IDR & BIT3)
+    {
+      // Do nothing until released
+    }
     Pause();
-  else if (config button)
+  }
+  else if (GPIOB_IDR & BIT4)
+  {
+    while (GPIOB_IDR & BIT4)
+    {
+      // Do nothing until released
+    }
     Config();
+  }
   else
     return 0;
 }
-
-// IRQ handler for the timer
+/*
+// IRQ handler for the timer (If we need it)
 int TimerIRQ
 {
   if (timer)
@@ -71,18 +125,18 @@ int TimerIRQ
   else
     return 0;
 }
-
+*/
 // Function to pause the alert system
 int Pause()
 {
-  if (PauseState == false)
+  if (PauseState == 0)
   {
-    PauseState = true;
+    PauseState = 1;
     GPIOB_ODR |= BIT0; // Turn on LED
   }
   else
   {
-    PauseState = false;
+    PauseState = 0;
     GPIOB_ODR &= ~BIT0; // Turn off LED
   }
   return 0;
@@ -110,12 +164,13 @@ int Config()
     GPIOB_ODR &= ~BIT1; // Turn off LED
     // Return to waiting
   }
+  return 0;
 }
-
+/*
 // Function to turn on buzzer
 int Buzzer()
 {
-  if (BuzzerStatus == false && PauseStatus == false)
+  if (BuzzerState == 0 && PauseState == 0)
   {
     GPIOA_ODR |= BIT2; // Turn on buzzer
     Timer();
@@ -127,15 +182,35 @@ int Buzzer()
   }
 }
 
-// Timer function
+// Timer function (Maybe just use a delay?)
 int Timer()
 {
   // Do timer stuff
   return 0;
 }
+*/
+
+void delay(int);
+
+void delay(int dly)
+{
+  while( dly--);
+}
 
 int main()
 {
+  if (FLASH_ACR == 1)
+  {
+    initClock();
+  }
+  PinSetup();
+  PauseState = 0;
+  ConfigState = 0;
+  BuzzerState = 0;
+  while(1)
+  {
+    ButtonIRQ();
+  }
 
   return 0;
 }
