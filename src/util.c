ec98845f3ab7e05c5e8daa545f9483344116f882
John Andersen <johnandersenpdx@gmail.com>
2017-12-01 18:35:38 -0800

    util: Fixed using c++ syntax
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- src/util.c
+++ src/util.c
@@ -4,7 +4,8 @@
  * For instance if bit 14 is set it will return 14.
  */
 int bit_index(uint32_t bit_check) {
-  for (unsigned int i = 0; i < 32; ++i) {
+  unsigned int i;
+  for (i = 0; i < 32; ++i) {
     if (bit_check & (1 << i)) {
       return i;
     }
